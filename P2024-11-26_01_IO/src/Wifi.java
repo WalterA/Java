1cf6b98a4a73a31a24c025a97ee7c4ba93cb92d1
WalterA <walteralb22@gmail.com>
2024-11-27 13:19:28 +0100

    java

--- P2024-11-26_01_IO/src/Wifi.java
+++ P2024-11-26_01_IO/src/Wifi.java
@@ -1,54 +1,57 @@
-public class Wifi {
+
+public class WI_FI {
 	private Double frequenza;
 	private String id;
 	private String password;
 	private String protocollo;
-	public Wifi(Double frequenza, String id, String password, String protocollo) {
-		super();
-		this.frequenza = frequenza;
-		this.id = id;
-		this.password = password;
-		this.protocollo = protocollo;
-	}
-	public Wifi() {
-		super();
-	}
-	@Override
-	public String toString() {
-		return "\nWifi [frequenza=" + frequenza + ", id=" + id + ", password=" + password + ", protocollo=" + protocollo
-				+ "]";
-	}
+
 	public Double getFrequenza() {
 		return frequenza;
 	}
+
 	public void setFrequenza(Double frequenza) {
 		this.frequenza = frequenza;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getProtocollo() {
 		return protocollo;
 	}
+
 	public void setProtocollo(String protocollo) {
 		this.protocollo = protocollo;
 	}
-	
-	public static Wifi MakeWifi() {
-		return new Wifi(
-				Util.GetFrequenza(), 
-				Util.GetPassword(false), 
-				Util.GetPassword(true), 
-				Util.GetRandomAuthProto());
-	}
 
+	public WI_FI(Double frequenza, String id, String password, String protocollo) {
+		super();
+		this.frequenza = frequenza;
+		this.id = id;
+		this.password = password;
+		this.protocollo = protocollo;
+	}
+	
+	@Override
+	public String toString() {
+		return "WI_FI [frequenza=" + frequenza + ", id=" + id + ", password=" + password + ", protocollo=" + protocollo
+				+ "]";
+	}
+	public static WI_FI MakeWifi() {
+		return new WI_FI(Util.getFrequenza(), Util.getPassword(false), Util.getPassword(true), Util.GetRandomAuthProto());
+		
+	}
 }
